8fd8e28c2a1b8c0eaf2d1d6ce3b88fdd3f57fefe
amrutha0842 <74587916+amrutha0842@users.noreply.github.com>
2021-01-15 12:58:56 +0000

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -6,7 +6,7 @@ class Pig extends BaseClass {
   }
 
  display(){
-   console.log(this.body.speed);
+   //console.log(this.body.speed);
    if(this.body.speed < 3){
     super.display();
    }
@@ -20,7 +20,12 @@ class Pig extends BaseClass {
    }
    
  }
-
+score(){
+  if(this.Visibility<0 && this.Visibility>-500){
+    score++
+    //console.log(score)
+  }
+}
 
 
 };
